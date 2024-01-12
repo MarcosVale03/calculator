919c541f58a731be992f3a7e4c588492db0a7a97
MarcosVale03 <116037001+MarcosVale03@users.noreply.github.com>
2024-01-11 19:03:38 -0800

    Update script.js

--- script.js
+++ script.js
@@ -21,19 +21,19 @@ function div(num1, num2) {
 // Calls an operation function depending on operator value
 function operate(num1, num2, operator) {
     if (operator === '+') {
-        add(num1, num2);
+        return add(num1, num2);
     }
 
     else if (operator === '-') {
-        sub(num1, num2);
+        return sub(num1, num2);
     }
 
     else if (operator === 'x') {
-        mult(num1, num2);
+        return mult(num1, num2);
     }
 
     else if (operator === '%') {
-        div(num1, num2);
+        return div(num1, num2);
     }
 }
 
@@ -42,7 +42,45 @@ function operate(num1, num2, operator) {
 let num1;
 let num2;
 let operator;
+let counter = 0;
 
+// Button operations are working, couple of things to remember
+// Check that the second button click is an operator and that
+// the first and third button clicks are not operators and are numbers
+// Account for decimal numbers, and then start displaying
 
+const buttons = document.querySelectorAll('button');
 
+buttons.forEach((button) => {
 
+    button.addEventListener('click', () => {
+
+        counter += 1;
+        
+        if (counter === 1) {
+
+            num1 = button.id;
+        }
+
+        else if (counter === 2) {
+            operator = button.id;
+        }
+
+        else if (counter === 3) {
+            num2 = button.id;
+        }
+        
+        else if (button.id === '=') {
+            num1 = parseFloat(num1);
+            num2 = parseFloat(num2);
+
+            alert(operate(num1,num2,operator))
+
+            counter = 0;
+            num1 = null;
+            num2 = null;
+            operator = null;
+        }
+        
+    })
+})
