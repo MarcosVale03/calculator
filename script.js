3eabb7e62ab7920996fe6d1279b2427019746038
MarcosVale03 <116037001+MarcosVale03@users.noreply.github.com>
2024-01-12 19:33:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Backend
 
 // Operation functions
-// check for float numbers later and 0 division error on div function
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -19,7 +19,8 @@ function div(num1, num2) {
 }
 
 // Calls an operation function depending on operator value
-function operate(num1, num2, operator) {
+
+function operate(num1, operator, num2) {
     if (operator === '+') {
         return add(num1, num2);
     }
@@ -38,16 +39,14 @@ function operate(num1, num2, operator) {
 }
 
 
-// Initializing variables 
+// Initializing variables
+
+const container = document.querySelector('#displayContainer')
+const container_backup = document.querySelector('#memContainer')
 let num1;
 let num2;
 let operator;
-let counter = 0;
-
-// Button operations are working, couple of things to remember
-// Check that the second button click is an operator and that
-// the first and third button clicks are not operators and are numbers
-// Account for decimal numbers, and then start displaying
+let equation = '';
 
 const buttons = document.querySelectorAll('button');
 
@@ -55,32 +54,52 @@ buttons.forEach((button) => {
 
     button.addEventListener('click', () => {
 
-        counter += 1;
+        if (button.id === 'clear') {
+            container.innerHTML = '';
+            equation = ''
+            return;
+        }
+
+        else if (button.id === 'delete') {
+            equation = equation.slice(0, -1);
+            container.innerHTML = equation;
+            
+        }
+
+        else if (button.id === '%' || button.id === 'x' || button.id === '-' || button.id === '+') {
+            equation += ' ' + button.id + ' ';
+            container.innerHTML = equation
+            
+        }
+
+        else if (button.id != '=') {
+
+            equation += button.id;
+            container.innerHTML = equation;
+        }
         
-        if (counter === 1) {
+        const eq_split = equation.split(' ');
 
-            num1 = button.id;
-        }
+        if (eq_split.length === 3 && button.id === '=') {
 
-        else if (counter === 2) {
-            operator = button.id;
-        }
+            container.innerHTML = operate(parseInt(eq_split[0]),eq_split[1],parseInt(eq_split[2]));
 
-        else if (counter === 3) {
-            num2 = button.id;
-        }
-        
-        else if (button.id === '=') {
-            num1 = parseFloat(num1);
-            num2 = parseFloat(num2);
-
-            alert(operate(num1,num2,operator))
-
-            counter = 0;
             num1 = null;
             num2 = null;
             operator = null;
+            equation = '';
+            
         }
         
     })
 })
+
+// Display should be bottom right
+
+// Dont let user click operators first or if an operator was already selected
+
+// check for float numbers later and 0 division error on div function
+
+// if user tries to do 13 + 2 + 2, solve 13 + 2 first 
+
+// Decimal input is EC, dont worry about it
