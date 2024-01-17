2595b13fbba650565abc79e0e07de728436086fc
MarcosVale03 <116037001+MarcosVale03@users.noreply.github.com>
2024-01-16 16:19:05 -0800

    Update script.js

--- script.js
+++ script.js
@@ -40,13 +40,13 @@ function operate(num1, operator, num2) {
 
 
 // Initializing variables
-
-const container = document.querySelector('#displayContainer')
-const container_backup = document.querySelector('#memContainer')
+const container = document.querySelector('#displayContainer');
+let backup;
 let num1;
 let num2;
 let operator;
 let equation = '';
+
 
 const buttons = document.querySelectorAll('button');
 
@@ -61,19 +61,41 @@ buttons.forEach((button) => {
         }
 
         else if (button.id === 'delete') {
-            equation = equation.slice(0, -1);
+
+            if (equation.slice(-1) === ' ') {
+                equation = equation.slice(0, -2)
+            }
+            else {
+                equation = equation.slice(0, -1);
+            }
+            
             container.innerHTML = equation;
             
         }
 
         else if (button.id === '%' || button.id === 'x' || button.id === '-' || button.id === '+') {
-            equation += ' ' + button.id + ' ';
-            container.innerHTML = equation
+            const eq_split = equation.split(' ');
+            if (eq_split.length === 3) {
+                container.innerHTML = '';
+                container.innerHTML = operate(parseInt(eq_split[0]),eq_split[1],parseInt(eq_split[2]));
+                
+                backup = container.innerHTML;
+
+                equation = '';
+                equation = backup + ' ' + button.id + ' ';
+        
+
+            }
+
+            else {
+                equation += ' ' + button.id + ' ';
+                container.innerHTML = equation;
+            }
+            
             
         }
 
         else if (button.id != '=') {
-
             equation += button.id;
             container.innerHTML = equation;
         }
@@ -82,7 +104,14 @@ buttons.forEach((button) => {
 
         if (eq_split.length === 3 && button.id === '=') {
 
-            container.innerHTML = operate(parseInt(eq_split[0]),eq_split[1],parseInt(eq_split[2]));
+            if (operate(parseInt(eq_split[0]),eq_split[1],parseInt(eq_split[2])) == 'Infinity') {
+                container.innerHTML = 'ZERO DIVISION ERROR';
+            }
+
+            else {
+                container.innerHTML = operate(parseInt(eq_split[0]),eq_split[1],parseInt(eq_split[2]));
+            }
+        
 
             num1 = null;
             num2 = null;
@@ -91,15 +120,6 @@ buttons.forEach((button) => {
             
         }
         
+        
     })
 })
-
-// Display should be bottom right
-
-// Dont let user click operators first or if an operator was already selected
-
-// check for float numbers later and 0 division error on div function
-
-// if user tries to do 13 + 2 + 2, solve 13 + 2 first 
-
-// Decimal input is EC, dont worry about it
